da37517ed1f2c68a68c926dcfb72fe0300f49484
jisub yoon <jisubyoon@jisubui-MacBook-Pro.local>
2018-09-21 20:21:21 +0900

    go swagger 추가 준비

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # require env
 go 1.11 
+
+
+go-swagger 추가 예정 
---
+++ src/TestTestCode/math/math.go
@@ -0,0 +1,33 @@
+package math
+
+import (
+	"errors"
+)
+
+// 값들을 모두 더한다.
+func Sum(nums ...int) int {
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	return total
+}
+
+// a를 b로 나눈다.
+func Div(a float64, b float64) (float64, error) {
+	if b == 0 {
+		return 0.0, errors.New("Can't divide by zero")
+	}
+	return a / b, nil
+}
+
+// 문자열을 count만큼 반복하고 결과를 반화한다.
+func StrRept(s string, count int) string {
+	b := make([]byte, len(s)*count)
+	bp := copy(b, s)
+	for bp < len(b) {
+		copy(b[bp:], b[bp:])
+		bp *= 2
+	}
+	return string(b)
+}
